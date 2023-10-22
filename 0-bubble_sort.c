15acf8e42c87d1b7733a9fd7d61168532c096ece
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-22 06:54:59 -0700

    bubble fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,12 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	size_t s = size;
-	int t;
+	int t, swapped;
 
 	while (s-- && s >= 1)
 	{
 		i = 0;
+		swapped = 0;
 		while (i < s)
 		{
 			if (array[i] > array[i + 1])
@@ -24,9 +25,12 @@ void bubble_sort(int *array, size_t size)
 				t = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = t;
-				print_array(array, size); 
+				print_array(array, size);
+			     	swapped = 1;
 			}
 			i++;
 		}
+		if (swapped == 0)
+			break;
 	}
 }
