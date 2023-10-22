62a0a0d1c9656dd9d20ef3aa876626b5ad18654b
RadouaneBaba <radoine.baba04@gmail.com>
2023-10-22 13:40:38 -0700

    select fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,22 +9,30 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int n;
+	int t, changed, n;
 
 	i = 0;
 	while (i < size)
 	{
+		changed = 0;
 		j = i + 1;
+		t = array[i];
 		while (j < size)
 		{
-			if (array[j] < array[i])
+			if (array[j] < t)
 			{
-				n = array[i];
-				array[i] = array[j];
-				array[j] = n;
-				print_array(array, size);
+				t = array[j];
+				changed = 1;
+				n = j;
 			}
 			j++;
+		}
+		if (changed == 1)
+		{
+			t = array[i];
+			array[i] = array[n];
+			array[n] = t;
+			print_array(array, size);
 		}
 		i++;
 	}
